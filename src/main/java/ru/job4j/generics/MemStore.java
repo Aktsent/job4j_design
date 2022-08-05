5ad2a10eb6cd76b98a4c84f4e5aba65920cde1a6
Aktsent <82117481+Aktsent@users.noreply.github.com>
2022-08-05 15:23:49 +0300

    MemStore

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -15,20 +15,22 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
+        boolean result = false;
         if (storage.containsKey(model.getId())) {
             storage.put(id, model);
             return true;
         }
-        return false;
+        return result;
     }
 
     @Override
     public boolean delete(String id) {
+        boolean result = false;
         if (storage.containsKey(id)) {
             storage.remove(id);
             return true;
         }
-        return false;
+        return result;
     }
 
     @Override
